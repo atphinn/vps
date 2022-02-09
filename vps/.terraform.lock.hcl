6406a6c667ddc13b9906b2136395c5f2d51ae2ef
atphinn <18540847+atphinn@users.noreply.github.com>
2022-02-09 17:41:38 -0500

    update vm

---
+++ vps/.terraform.lock.hcl
@@ -0,0 +1,39 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "2.95.0"
+  constraints = "~> 2.65"
+  hashes = [
+    "h1:YazJMhTUKbdoRUgZ1+UAcb5t8bcL3t4Apa3eqSEM9uk=",
+    "zh:09d2b8894e2c5df4e140d6ddf9224105b48b367afac6e10921c2571d53ae1d9c",
+    "zh:2974b8cd6633a8e6cb62f728fafdf1b6c656a27d3e4c050e94c142204c3893e4",
+    "zh:365ce52883a81f44021416a353d5370465317807344cf99a303718c97d226aa8",
+    "zh:54215ddfe1951ad705f0ce807851b8f3eb5aa2dc96a976e272f9ba027e239814",
+    "zh:5cfdacbf0d889cf22b8521eb9a732365b12f391cec1b3277c4ad1eaf40879ab6",
+    "zh:7086802525e280c6caf3538cd6cd72304acbdf85192756628db71a7d8d7aabc3",
+    "zh:8ea6d0d28120059d39e6a34bea8b7856f20896858fdef657dcfecd2c382cf3d1",
+    "zh:c21486433e5914403eca3344808c02ee5c5630256d9bc3f6f6750c90ecfedb5f",
+    "zh:dfebccd09c0d5f333d1a1a36bbd33b7abd5ff521143e81792ecf06dd3a6d0c0c",
+    "zh:e0b1d0190b0d5bd697d4398c5262864630bc36651fc73432bc7a3dc7bcb64758",
+    "zh:e410dc4301c27db2ea291b39e675b2ea741f058f0da01c7dc9d4accf61298dad",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/tls" {
+  version = "3.1.0"
+  hashes = [
+    "h1:XTU9f6sGMZHOT8r/+LWCz2BZOPH127FBTPjMMEAAu1U=",
+    "zh:3d46616b41fea215566f4a957b6d3a1aa43f1f75c26776d72a98bdba79439db6",
+    "zh:623a203817a6dafa86f1b4141b645159e07ec418c82fe40acd4d2a27543cbaa2",
+    "zh:668217e78b210a6572e7b0ecb4134a6781cc4d738f4f5d09eb756085b082592e",
+    "zh:95354df03710691773c8f50a32e31fca25f124b7f3d6078265fdf3c4e1384dca",
+    "zh:9f97ab190380430d57392303e3f36f4f7835c74ea83276baa98d6b9a997c3698",
+    "zh:a16f0bab665f8d933e95ca055b9c8d5707f1a0dd8c8ecca6c13091f40dc1e99d",
+    "zh:be274d5008c24dc0d6540c19e22dbb31ee6bfdd0b2cddd4d97f3cd8a8d657841",
+    "zh:d5faa9dce0a5fc9d26b2463cea5be35f8586ab75030e7fa4d4920cd73ee26989",
+    "zh:e9b672210b7fb410780e7b429975adcc76dd557738ecc7c890ea18942eb321a5",
+    "zh:eb1f8368573d2370605d6dbf60f9aaa5b64e55741d96b5fb026dbfe91de67c0d",
+    "zh:fc1e12b713837b85daf6c3bb703d7795eaf1c5177aebae1afcf811dd7009f4b0",
+  ]
+}
--- vps/main.tf
+++ vps/main.tf
@@ -15,12 +15,13 @@ provider "azurerm" {
   features {}
 }
 
+# Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "seetoothVPS" {
   name     = var.resource_group_name
   location = var.location
 }
 
-
+# Create virtual network
 resource "azurerm_virtual_network" "seetoothVPS" {
   name                = var.resource_group_name
   address_space       = ["10.0.0.0/22"]
@@ -28,7 +29,7 @@ resource "azurerm_virtual_network" "seetoothVPS" {
   resource_group_name = azurerm_resource_group.seetoothVPS.name
 }
 
-
+# Create subnet
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.seetoothVPS.name
@@ -36,6 +37,44 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+
+# Create public IPs
+resource "azurerm_public_ip" "myterraformpublicip" {
+  name                = "myPublicIP"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.seetoothVPS.name
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "VPS"
+  }
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "myterraformnsg" {
+  name                = "myNetworkSecurityGroup"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.seetoothVPS.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "VPS"
+  }
+}
+
+
+# Create network interface
 resource "azurerm_network_interface" "seetoothVPS" {
   name                = var.resource_group_name
   resource_group_name = azurerm_resource_group.seetoothVPS.name
@@ -48,13 +87,31 @@ resource "azurerm_network_interface" "seetoothVPS" {
   }
 }
 
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.seetoothVPS.id
+  network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+}
+
+
+# Create (and display) an SSH key
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+output "tls_private_key" {
+  value     = tls_private_key.example_ssh.private_key_pem
+  sensitive = true
+}
+
+# Create virtual machine
 resource "azurerm_linux_virtual_machine" "seetoothVPS" {
   name                            = var.resource_group_name
   resource_group_name             = azurerm_resource_group.seetoothVPS.name
   location                        = azurerm_resource_group.seetoothVPS.location
   size                            = "Standard_D2s_v3"
-  admin_username                  = "*******"
-  admin_password                  = "************************"
+  admin_username                  = ""
+  admin_password                  = ""
   disable_password_authentication = false
   network_interface_ids = [
     azurerm_network_interface.seetoothVPS.id,
